b1de6cb48230a75f16279c372ee4cc66eb0425a0
Costalesjonatan <costalesjonatan@gmail.com>
2025-07-09 17:25:35 -0300

    Se agrega pantalla nueva con imágenes, botones, se crea nuevo directorio para componentes reutilizables.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
-import { Image } from 'expo-image';
 import { StyleSheet, View } from 'react-native';
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -7,7 +9,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -17,14 +23,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+   footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  }
 });
